78427c68940b13ff730094ec58cb5cc0139186e2
edisj <>
2021-01-06 05:33:49 -0700

    commented out couple runs

--- benchmarks/masked_array_1/scripts/job_submit.sh
+++ benchmarks/masked_array_1/scripts/job_submit.sh
@@ -9,8 +9,8 @@ do
     sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/1node28.sh $1 $repeat
     sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/2node56.sh $1 $repeat
     sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/4node112.sh $1 $repeat
-    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/6node168.sh $1 $repeat
+    #sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/6node168.sh $1 $repeat
     sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/8node224.sh $1 $repeat
-    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/12node336.sh $1 $repeat
+    #sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/12node336.sh $1 $repeat
     sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/copy_file.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/masked_array_1/scripts/16node448.sh $1 $repeat
 done
