b369262f56adbc380ed51645e8a5019d113cfe70
edisj <>
2021-01-20 12:12:56 -0700

    more striping job scripts

--- benchmarks/striping/scripts/job_submit_2000.sh
+++ benchmarks/striping/scripts/job_submit_2000.sh
@@ -5,6 +5,8 @@ cd /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/results/$1/slurm_output
 
 for repeat in 1 2 3
 do
-    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/8node224_2000.sh $1 $repeat
-    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/16node448_2000.sh $1 $repeat
+    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/1node1_2000.sh $1 $repeat
+    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/1node28_2000.sh $1 $repeat
+    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/2node56_2000.sh $1 $repeat
+    sbatch /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/copy_file_2000.sh /pylon5/mc4sb2p/edisj/Bridges/benchmarks/striping/scripts/4node112_2000.sh $1 $repeat
 done
